f21be8ceda231c683dc8a76b7ae43fdb75164ba5
IqraRehmani <ahmadrehamni9206@gmail.com>
2024-07-30 13:54:28 +0500

    added components

---
+++ frontend/src/components/SidePanel.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import painting from "../assets/images/painting.png";
+
+function SidePanel() {
+  return (
+    <>
+      <div className="   w-full hidden   p-5 bg-gradient-to-t from-primary-900 to-primary-600  md:w-[50%]  md:flex  md:flex-col justify-around">
+        <div className="w-[100%] text-center">
+          <h1 className=" text-secondary-400 font-bold text-[2.3rem] ">
+            PAINTORA
+          </h1>
+          <p className=" text-white font-semibold text-[1.6rem] ">
+            We Are Here For
+          </p>
+          <p className=" text-white font-semibold text-[1.6rem]  ">
+            Uniting Painters With{" "}
+            <span className=" text-secondary-400 "> Passion And Purpose </span>
+          </p>
+        </div>
+        <img src={painting} alt="this is an image" className="w-[100%]  " />
+      </div>
+    </>
+  );
+}
+
+export default SidePanel;
---
+++ frontend/src/components/loginForm/Login.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import SidePanel from "../SidePanel";
+
+function Login() {
+  // state for input fields
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  //function to handle the inputs
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(name, value);
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  // function is ended
+  return (
+    <div className=" w-[80%] flex flex-col overflow-hidden bg-gray-50 rounded-[20px] shadow-xl h-[95vh] mx-auto my-5 md:w-[60%] md:flex-row ">
+      {/* left side panel */}
+      <SidePanel />
+
+      {/* right side form */}
+      <div className=" w-full  p-10 flex flex-col items-center justify-center md:w-[50%] ">
+        <form action="" className="flex flex-col items-center w-[100%] h-fit ">
+          <h1 className="text-black text-[1.6rem] font-medium mb-4 ">
+            Welcome Back
+          </h1>
+          <div className="w-[100%]">
+            <label htmlFor=""></label>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={data.email}
+              onChange={handleChange}
+              className="w-full p-2 border-[2px] border-gray-200 rounded-lg outline-primary-300 hover:border-primary-400 my-2 bg-transparent text-[0.9rem]"
+            />
+          </div>
+          <div className="w-[100%] my-2">
+            <label htmlFor=""></label>
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={data.password}
+              onChange={handleChange}
+              className="w-full p-2 border-[2px] border-gray-200 rounded-lg outline-primary-300 hover:border-primary-400  bg-transparent text-[0.9rem]"
+            />
+            <a href="#" className="text-[0.9rem] text-black">
+              Forgot Password?
+            </a>
+          </div>
+          <button
+            type="submit"
+            value="submit"
+            className="w-[100%] p-2 text-white bg-gradient-to-t from-primary-800 to-primary-400 hover:shadow-lg rounded-lg my-2 font-medium "
+          >
+            Login
+          </button>
+          <div className="flex flex-row my-1 gap-1">
+            <p className="text-[0.9rem]">Do not have an account?</p>
+            <a href="#" className="text-[0.9rem] text-primary-900 underline">
+              Signup
+            </a>
+          </div>
+        </form>
+
+        {/* other Login options */}
+        <div className="w-[100%] flex flex-row gap-3 items-center justify-center my-2">
+          <hr className="flex-1 border-t-1 border-gray-400" />
+          <span className="text-[0.9] text-gray-500">or</span>
+          <hr className="flex-1 border-t-1 border-gray-400" />
+        </div>
+        <div className="flex flex-col items-center w-[100%] h-fit ">
+          <div className="w-[100%] my-3">
+            <button className="w-[100%] h-fit p-2  border-[2px] border-gray-200 rounded-lg hover:border-primary-400 hover:bg-gray-100 text-[0.9rem]">
+              {" "}
+              Continue With Google
+            </button>
+          </div>
+          <div className="flex flex-row gap-4 w-[100%] my-3">
+            <button className="w-[50%] h-fit p-2  border-[2px] border-gray-200  rounded-lg hover:border-primary-400 hover:bg-gray-100 text-[0.9rem]">
+              Facebook
+            </button>
+            <button className="w-[50%] h-fit p-2  border-[2px] border-gray-200 rounded-lg hover:border-primary-400 hover:bg-gray-100 text-[0.9rem]">
+              LinkedIn
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
