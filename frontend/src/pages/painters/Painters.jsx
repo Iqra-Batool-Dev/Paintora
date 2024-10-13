a9e6c8b54d02bdaeb2735b62ea405ddbc6e3334e
IqraRehmani <ahmadrehamni9206@gmail.com>
2024-10-13 23:25:42 +0500

    changes in frontend

--- frontend/src/pages/painters/Painters.jsx
+++ frontend/src/pages/painters/Painters.jsx
@@ -3,40 +3,52 @@ import { useState , useEffect } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import paintersData from "../mockData/painters.json"
+import citiesData from "../mockData/cities.json"
 
 function Painters() {
     const categories = [ "Interior Painting","Flauk Painting", "Exterior Painting",   "Furniture Painting", "Commercial Painting" ]
-    const [isActive , setIsActive] = useState(null)
+    
     const [searchHeading , setSearchHeading] = useState('All Painters')
-
-    // function to handle category buttons
-    const [category , setCategory] = useState('')
-    const handleButton = (e)=>{
-        const value= e.target.value
-        setCategory(value)
-        setSearchHeading(value)
-        console.log(value)
-    }
-    // function to handle search bar//
     const [search , setSearch] = useState('')
-    // const handleSearch =()=>{
-    //     const searchResults = 
-
-    // }
-
-            const searchResults = paintersData.filter(item => item.speciality.toString().toLowerCase().includes(category.toLowerCase()))
-            // const searchResults = paintersData.filter(item => /[item.speciality | category]/ )
+    const [category , setCategory] = useState('')
+    const [location , setLocation] = useState('')
+    const [finalResults, setFinalResults] =useState([])
     
+        // function to handle category buttons
+        const handleButton = (e)=>{
+            const value= e.target.value
+            setCategory(value)
+            setSearchHeading(value)
+            console.log(value)
+            const searchResults = paintersData.filter(item => item.location.toString().toLowerCase().includes(category.toLowerCase()))
+            setFinalResults(searchResults)
+        }
+        // function to handle search bar//
+        const handleSearch =(e)=>{
+            if(e.key === "Enter"){
+                setSearch(e.target.value)
+            }
     
+        }
+    
+        // function to perform search 
+        
+                
+
+
     return (
-    <div className=' w-[100%] py-4 px-8'> 
+    <div className=' w-[100%] py-4 px-10'> 
         <div className=' search-heading my-5 text-[1.8rem] text-black font-bold'>{searchHeading}</div>
 
         {/* category buttons */}
         <div className={`  w-[100%] flex flex-wrap gap-2 `}>
         {
             categories.map((category, index)=>( 
-                <button className={`px-3 py-2 text-[0.9rem] font-semibold hover:bg-gray-300 rounded-full  bg-gray-200 `} key={index} value={category}  onClick={(e)=>handleButton(e)}>
+                <button 
+                className={`px-3 py-2 text-[0.9rem] font-semibold hover:shadow-md  rounded-full  bg-gray-100 `} 
+                key={index} 
+                value={category}  
+                onClick={(e)=>handleButton(e)}>
                 {category}
                 </button>
             ))
@@ -44,22 +56,45 @@ function Painters() {
         </div>
 
         {/* search bar  */}
-        <div className="  border-[2px] border-gray-200 rounded-lg flex flex-row justify-around items-center px-2 hover:border-primary-300 bg-white">
+        <div className=' flex flex-row justify-between my-10 '>
+        <div className="  border-[2px] border-gray-200 rounded-lg flex flex-row justify-between items-center gap-2 px-3 hover:border-primary-300 bg-white w-[60%]">
             <FontAwesomeIcon icon={faSearch} className="text-gray-400 " />
             <input
                 type="search"
-                placeholder="Search..."
+                placeholder="Search by keywords, skills, tags...."
                 name="search"
-                className="text-[1rem] text-gray-950 py-1 px-4  outline-none  "
+                className="text-[1rem] text-gray-950 py-2 w-[100%]  outline-none  "
                 value={search} 
                 onChange={(e)=>{setSearch(e.target.value)}}
-                // onKeyDown={handleSearch}
+                onKeyDown={handleSearch}
             />
+
+        {/* select menu */}
+        </div>
+        <select name="select" id="select" filter = "true" className=' py-1 px-5 border-[2px]  rounded-md outline-none border-gray-200 hover:border-primary-300 text-gray-400' 
+        onChange={(e)=>handleLocation(e)}>
+        <option selected disabled className='text-gray-300 text-center'>Search by location</option>
+            {citiesData.map((city , index)=>(    
+                <option 
+                    value={location}
+                    key={index} 
+                    className=' text-[0.9rem] ' 
+                    defaultValue="location" 
+                    >
+                    {city}
+                </option>
+            ))
+            }
+        </select>
+    
         </div>
 
-
+        {/* painter cards section */}
         <div>
-            {searchResults.map((item , index)=>(
+
+            { 
+                
+                finalResults.map((item , index)=>(
                 <h1 key={index}>{item.name}</h1>
             ))}
         </div>
